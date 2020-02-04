a56b1f21bf384a187d4a7fef288fa7ec1c789019
Pavel Rubin <pash7ka@gmail.com>
2020-02-04 23:02:20 +0300

    Make minting continuous with hard cap

--- contracts/KTYtoken.sol
+++ contracts/KTYtoken.sol
@@ -1,14 +1,27 @@
 pragma solidity ^0.5.0;
 
 import './openzeppelin_v2_4_0/token/ERC777/ERC777.sol';
+import './openzeppelin_v2_4_0/ownership/Ownable.sol';
 
-contract KTYtoken is ERC777 {
+contract KTYtoken is Ownable, ERC777 {
     string constant NAME    = 'Kittiefight';
     string constant SYMBOL  = 'KTY';
-    uint256 constant TOTAL_SUPPLY = 100_000_000 * 10**18;
+    uint256 constant MAX_TOTAL_SUPPLY = 100_000_000 * 10**18;
 
     constructor() ERC777(NAME, SYMBOL, new address[](0)) public {
-        _mint(msg.sender, msg.sender, TOTAL_SUPPLY, '', '');
+    }
+
+    /**
+     * @notice Mint new KTY
+     * It also verifies that MAX_TOTAL_SUPPLY will not be exeeded by this call
+     * @param account Address to send minted tokens
+     * @param amount Amount of tokens to mint
+     * @return Should always return true
+     */
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(totalSupply().add(amount) <= MAX_TOTAL_SUPPLY, "KTYtoken: cap exceeded");
+        _mint(_msgSender(), account, amount, '', '');
+        return true;
     }
 }
 
